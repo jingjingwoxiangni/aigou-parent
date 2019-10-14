1fd5b283814245a20f1a4a28f29f490f3886599e
solargen <solargen@qq.com>
2019-10-14 16:41:20 +0800

    商品类型优化

--- aigou-product-parent/product-service/src/main/java/cn/itsource/aigou/ProductApplicaiton.java
+++ aigou-product-parent/product-service/src/main/java/cn/itsource/aigou/ProductApplicaiton.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootApplication
 @MapperScan("cn.itsource.aigou.mapper")
+@EnableFeignClients
 public class ProductApplicaiton {
 
     public static void main(String[] args) {
